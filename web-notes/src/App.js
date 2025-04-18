9e2bd5c283d9d81eeecb1f3ed40ead6da1b87a03
MiiKel36 <mimaporcel.dev01@gmail.com>
2025-04-18 02:15:47 -0300

    First commit

---
+++ web-notes/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <div className='header'>
+        <img className='img_logo' src='https://placehold.co/55'/>
+        <p className='title'>Notes</p>
+      </div>
+      <br/>
+      <div className='notes'>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
